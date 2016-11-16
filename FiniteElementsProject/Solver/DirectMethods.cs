857cf35c8747127e19f951eef3432f2e317a226d
Vasilis Merevis <vasilis.merevis@outlook.com>
2016-11-16 19:07:42 +0200

    Addition of new interfaces and classes. Breaking Solver to smaller files.

---
+++ FiniteElementsProject/Solver/DirectMethods.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FiniteElementsProject
+{
+    abstract class DirectMethods
+    {
+        protected double[] solutionVector;
+        protected double[,] stiffnessMatrix;
+        protected double[] forceVector;
+
+        protected double[] BackSubstitution(double[,] upperTriangMatrix, double[] forceVector)
+        {
+            int rows = forceVector.GetLength(0);
+            double[] solutionVector = new double[rows];
+            double total;
+
+            for (int i = rows - 1; i >= 0; i--)
+            {
+                total = forceVector[i];
+                if (i < rows - 1)
+                {
+                    for (int j = i + 1; j < rows; j++)
+                    {
+                        total = total - upperTriangMatrix[i, j] * solutionVector[j];
+                    }
+                }
+                solutionVector[i] = total / upperTriangMatrix[i, i];
+            }
+            return solutionVector;
+        }
+
+        protected double[] ForwardSubstitution(double[,] lowerTriangMatrix, double[] forceVector)
+        {
+            int rows = forceVector.GetLength(0);
+            double[] solutionVector = new double[rows];
+            double total;
+            for (int i = 0; i < rows; i++)
+            {
+                total = forceVector[i];
+                if (i > 0)
+                {
+                    for (int j = 0; j <= i - 1; j++)
+                    {
+                        total = total - lowerTriangMatrix[i, j] * solutionVector[j];
+                    }
+                }
+                solutionVector[i] = total / lowerTriangMatrix[i, i];
+            }
+            return solutionVector;
+        }
+    }
+}
---
+++ FiniteElementsProject/Solver/GaussElimination.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FiniteElementsProject
+{
+    class Gauss : DirectMethods, IDirectSolver
+    {
+        private void GaussElimination(double[,] matrix, double[] vector)
+        {
+            for (int k = 0; k < vector.Length - 1; k++)
+            {
+                for (int i = k + 1; i < vector.Length; i++)
+                {
+                    for (int j = k + 1; j < vector.Length; j++)
+                    {
+                        matrix[i, j] = matrix[i, j] - (matrix[i, k] / matrix[k, k]) * matrix[k, j];
+                    }
+                    vector[i] = vector[i] - (matrix[i, k] / matrix[k, k]) * vector[k];
+                    matrix[i, k] = 0;
+                }
+            }
+        }
+
+        public void Solve()
+        {
+            GaussElimination(stiffnessMatrix, forceVector);
+            this.solutionVector = BackSubstitution(stiffnessMatrix, forceVector);
+        }
+    }
+}
