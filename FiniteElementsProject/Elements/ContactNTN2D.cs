9e54e083fedb42a0dbe6d2e832c03ab29f27f0d5
VasilisMerevis <user@VASILIS-NETBOOK>
2017-02-27 18:57:14 +0200

    Creation of Contact2DNTN element

---
+++ FiniteElementsProject/Elements/ContactNTN2D.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FiniteElementsProject
+{
+    class ContactNTN2D : Element1D
+    {
+        private double PenaltyFactor { get; set; }
+        public ContactNTN2D(double E, double A, double[] nodesX, double[] nodesY)
+            : base(E, A, nodesX, nodesY)
+        {
+            lambdaMatrix = new double[4, 4];
+            localStiffnessMatrix = new double[6, 6];
+            globalStiffnessMatrix = new double[4, 4];
+        }
+
+        private double[] CalculateNormalUnitVector()
+        {
+
+        }
+
+        public override double[,] CreateGlobalStiffnessMatrix()
+        {
+            
+        }
+    }
+}
